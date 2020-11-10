2ea372e182956866d55f3013f72be98d930c76c0
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2020-11-11 07:23:35 +0900

    たぶんやりたいことはこういうことだと思うんですけど

--- src/main/java/jp/ac/kyoto_su/cse/simpleapp/Main.java
+++ src/main/java/jp/ac/kyoto_su/cse/simpleapp/Main.java
@@ -1,14 +1,20 @@
 package jp.ac.kyoto_su.cse.simpleapp;
 
+
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
 
 import jp.cafebabe.pochi.BirthmarkSystemHelper;
+import jp.cafebabe.birthmarks.extractors.Extractor;
+import jp.cafebabe.kunai.source.DataSource;
+import jp.cafebabe.birthmarks.entities.Birthmarks;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.io.File;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -21,26 +27,24 @@ public class Main {
 
         JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
 
-        BirthmarkSystemHelper bsh = new BirthmarkSystemHelper();
 
-        // int slices = (args.length == 1) ? Integer.parseInt(args[0]) : 2;
-        int n = 1;
-        List<Integer> l = new ArrayList<>(n);
-        for (int i = 0; i < n; i++) {
-            l.add(i);
-        }
+        List<File> list = new ArrayList<>();
+        File directory = new File("/path/to/jars");
+        list = Arrays.asList(Objects.requireNonNull(directory.listFiles()));
 
-        JavaRDD<Integer> dataSet = jsc.parallelize(l);
+        JavaRDD<File> dataSet = jsc.parallelize(list);
 
-        List<String> ret = dataSet.map(integer -> {
-            return Arrays.asList(bsh.extractorNames());
-        }).reduce((str1, str2) -> {
-            str1.addAll(str2);
-            return str1;
-        });
+        // BirthmarkSystemHelper bsh = new BirthmarkSystemHelper();
 
-        ret.forEach(System.out::println);
-        // System.out.println("Pi is roughly " + 4.0 * count / n);
+
+        Birthmarks ret = dataSet.map(file -> {
+            BirthmarkSystemHelper bsh = new BirthmarkSystemHelper();
+            DataSource source = bsh.source(file.getAbsolutePath());
+            Extractor extractor = bsh.extractor("uc");
+            return extractor.extract(source);
+        }).reduce(Birthmarks::merge);
+
+        System.out.println(ret.toString());
 
         spark.stop();
 
